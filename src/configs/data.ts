b04cd628b5a222ee3aa290d87b632bce8f5c4490
Danil Khegai <danilmaster95@gmail.com>
2024-03-03 22:08:25 +0500

    2024/03/04 - внесены корректировки по замечаниям

--- src/configs/data.ts
+++ src/configs/data.ts
@@ -1,15 +1,9 @@
 import { FirstFairyTale } from './fairy_tales/FirstFairyTale';
-import { SecondFairyTale } from './fairy_tales/SecondFaityTale';
 
 export const DATA = {
   FIRST_FAIRY_TALE: {
     id: 0,
     name: 'Сказка о Риме',
     data: FirstFairyTale,
-  },
-  SECOND_FAIRY_TALE: {
-    id: 1,
-    name: 'Сказка о латинской грамматике',
-    data: SecondFairyTale,
   },
 };
